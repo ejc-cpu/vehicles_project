5294032ffb6f2de912c1d27b63becb5252e4808d
Jesus Vive <ejccab@hotmail.com>
2024-07-25 16:21:19 -0400

    cambios finales

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # vehicles_project
 Proyecto sprint 5
+Esta aplicación web, desarrollada con Streamlit, proporciona una interfaz interactiva para visualizar datos de vehículos mediante gráficos.
+Al hacer clic en un botón, se genera un histograma que muestra la distribución de los valores del odómetro de los vehículos en el conjunto de datos. Otro botón permite al usuario construir un gráfico de dispersión que visualiza la relación entre el odómetro y el precio de los vehículos.
+
+
+[Enlace](https://vehicles-project-icru.onrender.com)
--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 # %%
 import pandas as pd
 import plotly_express as px
+import streamlit as st
 
 # %%
 car_data = pd.read_csv(r'.\vehicles_us.csv')
-fig = px.histogram(car_data, x='odometer')
-fig.show()
+
+
+st.header('Cuadro de mandos')
+hist_button = st.button('Construir histograma')
+
+if hist_button:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x='odometer')
+    st.plotly_chart(fig, use_container_width=True)
+
+disp_button = st.button('construir grafico de dispersion')
+if disp_button:
+    st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x='odometer', y='price')
+    st.plotly_chart(fig, use_container_width=True)
